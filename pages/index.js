3dc573173cc0bd5741b60cb1636107d3904fea2f
suga-ming <kby9901@naver.com>
2023-04-19 17:02:29 +0900

    Feat(index):hero 추가

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import Layout from "@/components/layout";
+import Hero from "@/components/home/hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,25 +16,7 @@ export default function Home() {
       </Head>
       <section className="flex min-h-screen flex-col items-center text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-3xl text-3xl mb-4 font-medium text-gray-900">
-              안녕하세요! 😊
-              <br className="hidden lg:inline-block" />
-              프론트엔드 개발자 김보영입니다.
-            </h1>
-            <p className="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-              park mlkshk tote bag selvage hot chicken authentic tumeric
-              truffaut hexagon try-hard chambray.
-            </p>
-            <div className="flex justify-center">
-              <button className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
-                프로젝트 보러가기
-              </button>
-            </div>
-          </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"></div>
+          <Hero />
         </div>
       </section>
     </Layout>
