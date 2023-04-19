1d63b2b70dcf37bd487e0a75e0cd69aa1fcf2b4c
suga-ming <kby9901@naver.com>
2023-04-19 17:49:27 +0900

    Feat(hero):link 연결

--- components/home/hero.js
+++ components/home/hero.js
@@ -1,4 +1,5 @@
 import Animation from "@/components/animation";
+import Link from "next/link";
 
 export default function Hero() {
   return (
@@ -16,9 +17,11 @@ export default function Hero() {
           try-hard chambray.
         </p>
         <div className="flex justify-center">
-          <button className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
-            프로젝트 보러가기
-          </button>
+          <Link legacyBehavior href="/projects">
+            <a className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+              프로젝트 보러가기
+            </a>
+          </Link>
         </div>
       </div>
       <div className="flex justify-center lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
