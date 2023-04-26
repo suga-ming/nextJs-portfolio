412d02ee6e65050c05a10c072f87cb5fdcfeba10
suga-ming <kby9901@naver.com>
2023-04-26 17:31:34 +0900

    Fix(toggle-button):다크/라이트 모드 오류 해결

--- components/dark-mode-toggle-button.js
+++ components/dark-mode-toggle-button.js
@@ -9,8 +9,8 @@ export default function DarkModeToggleBoutton() {
         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         className="inline-flex items-center bg-gray-100 dark:bg-slate-500 dark:text-yellow-50 px-2 py-1 border-0 focus:outline-none hover:bg-gray-50 hover:text-orange-400 hover:dark:text-yellow-300 hover:dark:bg-slate-500 rounded text-base mt-4 md:mt-0"
       >
-        {/* 라이트 모드 */}
-        {theme === "light" && (
+        {/* 라이트/다크모드 */}
+        {theme === "light" ? (
           <svg
             className="w-6 h-6"
             fill="currentColor"
@@ -20,10 +20,7 @@ export default function DarkModeToggleBoutton() {
           >
             <path d="M10 2a.75.75 0 01.75.75v1.5a.75.75 0 01-1.5 0v-1.5A.75.75 0 0110 2zM10 15a.75.75 0 01.75.75v1.5a.75.75 0 01-1.5 0v-1.5A.75.75 0 0110 15zM10 7a3 3 0 100 6 3 3 0 000-6zM15.657 5.404a.75.75 0 10-1.06-1.06l-1.061 1.06a.75.75 0 001.06 1.06l1.06-1.06zM6.464 14.596a.75.75 0 10-1.06-1.06l-1.06 1.06a.75.75 0 001.06 1.06l1.06-1.06zM18 10a.75.75 0 01-.75.75h-1.5a.75.75 0 010-1.5h1.5A.75.75 0 0118 10zM5 10a.75.75 0 01-.75.75h-1.5a.75.75 0 010-1.5h1.5A.75.75 0 015 10zM14.596 15.657a.75.75 0 001.06-1.06l-1.06-1.061a.75.75 0 10-1.06 1.06l1.06 1.06zM5.404 6.464a.75.75 0 001.06-1.06l-1.06-1.06a.75.75 0 10-1.061 1.06l1.06 1.06z"></path>
           </svg>
-        )}
-
-        {/* 다크모드 */}
-        {theme === "dark" && (
+        ) : (
           <svg
             className="w-6 h-6"
             fill="currentColor"
@@ -32,8 +29,8 @@ export default function DarkModeToggleBoutton() {
             aria-hidden="true"
           >
             <path
-              clip-rule="evenodd"
-              fill-rule="evenodd"
+              clipRule="evenodd"
+              fillRule="evenodd"
               d="M7.455 2.004a.75.75 0 01.26.77 7 7 0 009.958 7.967.75.75 0 011.067.853A8.5 8.5 0 116.647 1.921a.75.75 0 01.808.083z"
             ></path>
           </svg>
